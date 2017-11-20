8e1fc474a78aeb6d2cf9449bf91a74ebbdb491ea
Finneyyy <eoin.finney@gmail.com>
2017-11-20 18:48:11 +0000

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace EFProject;
+
+?>
+
+
+<head>
+
+<meta charset="utf-8">
+
+<title>Home</title>
+
+	<style>
+		@import '../web/css/intro.css';
+		@import '../web/css/layout.css';
+		@import '../web/css/navbar.css';
+		
+	</style>
+	
+	<div class="backImage">
+		<img src="../web/images/homePage.jpg">
+	</div>
+	
+</head>
+
+<header>
+	<nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="about.php">About Author</a></li>
+			<li><a href="mainChars.php">Main Characters</a></li>
+			<li><a href="places.php">Place + Setting</a></li>
+			<li><a href="story.php">Story - Spoilers</a></li>
+			<li><a href="buyNow.php">Buy Now!</a></li>
+			
+		</ul>
+	</nav>
+</header>
+
+
+<body>
+	
+</body>
