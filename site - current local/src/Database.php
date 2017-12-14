aa2133ffc3c8cac6776c7d617baccbede17abafc
Finneyyy <>
2017-12-14 15:43:22 +0000

    Update
    Changed Database and ProductRepo php files

--- site - current local/src/Database.php
+++ site - current local/src/Database.php
@@ -3,38 +3,38 @@ namespace EFProject;
 
 class Database
 {
-	const DB_NAME = 'users';
-	const DB_USER = 'staff';
-	const DB_PASS = 'staff';
+	const DB_NAME = 'it';
+	const DB_USER = 'stephen';
+	const DB_PASS = 'king';
 	const DB_HOST = 'localhost:8000';
-	
+
 	//private connection
 	private $connection;
-	
+
 	//create & store MySQL db connection
 	public function __construct()
 	{
 		try
 		{
 			$this->connection = $this->createMysqlConnection();
-		} 
+		}
 		catch(\Exception $e)
 		{
 			print $e;
 		}
 	}
-	
+
 	//create new MySQL db connection
 	private function createMysqlConnection()
 	{
 		$dsn = 'mysql:dbname' . self::DB_NAME . ';host= ' . self::DB_HOST;
 		return new \PDO($dsn, self::DB_USER, self::DB_PASS);
 	}
-	
+
 	//@return mixed
 	public function getConnection()
 	{
 		return $this->connection;
 	}
-	
+
 }
--- site - current local/src/ProductRepository.php
+++ site - current local/src/ProductRepository.php
@@ -46,7 +46,84 @@ class ProductRepository
   {
     $sql = 'delete from Product';
 
-    
+    $rowAffex = $this->connection->exec($sql);
+    return $rowAffex;
+  }
+
+  public function insertIntoProduct($description, $price)
+  {
+    $sql = 'insert into Product (description, price) values (:description, :amount)';
+    $stmt = $this->connection->prepare($sql);
+
+    $stmt->bindParam(':description', $description);
+    $stmt->bindParam(':amount', $price);
+
+    //todo: Figure out proper comment
+    $noErrors = $stmt->execute();
+
+    if($noErrors)
+    {
+      return $this->connection->lastIdInserted();
+    } else
+    {
+      return false;
+    }
+  }
+
+  public function updateProduct($id, $description, $price)
+  {
+    $sql = "update Product set description = :description, price = :price where id = :id";
+
+    $stmt = $this->connection->prepare($sql);
+
+    //Start to bind, preferably without ropes
+    $stmt->bindParam(':id', $id);
+    $stmt->bindParam(':description', $description);
+    $stmt->bindParam(':price', $price);
+
+    //Filler
+    $noErrors = $stmt->execute();
+    return $noErrors;
+  }
+
+  public function getOneFromProduct($id)
+  {
+    //$sql = 'select * from Product where $id = :id'
+    $sql = 'select * from Product where id = :id';
+    $stmt = $this->connection->prepare($sql);
+
+    //Bound with wrists behind back
+    $stmt->bindParam(':id', $id);
+
+    //EXECUTE HIM
+    $stmt->execute();
+
+    //Fetch boy, go fetch
+    $stmt->setFetchMode(\PDO::FETCH_CLASS, 'EFProject\\Product');
+
+    if($product = $stmt->fetch())
+    {
+      return $product;
+    } else
+    {
+      return null;
+    }//end else
+
+    return $product;
+  }
+
+  public function removeOneFromProduct($id)
+  {
+    $sql = 'delete from Product where id = :id';
+    $stmt = $this->connection->prepare($sql);
+
+    //How do I always manage to get tied up?
+    $stmt->bindParam(':id', $id);
+
+    //*sighs*.. Another one? What did he do?
+    $noErrors = $stmt->execute();
+
+    return $noErrors;
   }
 
 
