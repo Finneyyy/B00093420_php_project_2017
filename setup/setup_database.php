696bfe06adb3301da4e0271cfd82ae59795fe95d
Finneyyy <>
2017-12-14 10:45:22 +0000

    Update - 14/12/17

--- setup/setup_database.php
+++ setup/setup_database.php
@@ -6,7 +6,17 @@ use EFProject\ProductRepository;
 //have to create the database first
 $productRepository = new ProductRepository();
 
-$productRepository->dropTable();
-$productRepository->createTable();
+$productRepository->dropProductTable();
+$productRepository->createProductTable();
+
+$productRepository->deleteAll();
+$productRepository->getAllFromProduct();
+
+$productRepository->insertIntoProduct('Hoodie', 25.00);
+$productRepository->insertIntoProduct('T-Shirt', 10.00);
+$productRepository->insertIntoProduct('Sweatshirt', 20.00);
+$productRepository->insertIntoProduct('Mug', 5.00);
+$productRepository->insertIntoProduct('Book', 16.99);
+
 
 //$productRepository->deleteAll(); //this goes into the LoginController in ref. to specific users (admin).
